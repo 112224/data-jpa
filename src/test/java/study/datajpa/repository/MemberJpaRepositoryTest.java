7d65794ca0f79c5d328f07144f20fb58047e997e
112224 <didgns25@naver.com>
2023-02-25 22:59:54 +0900

    Feat : Member Entity 개발
    간단한 테스트 코드 작성

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    @Rollback(value = false)
+    public void testMember() {
+
+        Member member = new Member("hoon");
+
+        Member save = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(save.getId());
+
+        assertThat(save.getId()).isEqualTo(findMember.getId());
+        assertThat(save.getUserName()).isEqualTo(findMember.getUserName());
+        assertThat(save).isEqualTo(findMember);
+    }
+}
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Rollback(value = false)
+    public void testMember() {
+
+        Member member = new Member("hoon");
+
+        Member save = memberRepository.save(member);
+        Member findMember = memberRepository.findById(save.getId()).get();
+
+        assertThat(save.getId()).isEqualTo(findMember.getId());
+        assertThat(save.getUserName()).isEqualTo(findMember.getUserName());
+        assertThat(save).isEqualTo(findMember);
+    }
+}
