b735c3d2f73b001dcba6b79b71d5a3cfc257036a
112224 <didgns25@naver.com>
2023-02-27 05:23:53 +0900

    Feat : Member, Team JPA repository 개발
    기본적인 CRUD 를 지원하는 순수 JPA repository 개발

--- src/main/java/study/datajpa/domain/member/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/domain/member/repository/MemberJpaRepository.java
@@ -1,9 +1,13 @@
 package study.datajpa.domain.member.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import study.datajpa.domain.member.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -14,6 +18,24 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public Long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
     }
 
     public Member find(Long id) {
