df50e78d5a8ced6646687dc65aeadf60d593626f
112224 <didgns25@naver.com>
2023-03-07 04:44:03 +0900

    Feat : MemberList 응답 API 개발
    Pageable interface 를 이용하여 Paging 과 정렬을 제공.
    Dto 의 생성자 중 MemberEntitiy 를 파라미터로 받는 것 추가

--- src/main/java/study/datajpa/domain/member/MemberController.java
+++ src/main/java/study/datajpa/domain/member/MemberController.java
@@ -3,12 +3,17 @@ package study.datajpa.domain.member;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.domain.member.dto.MemberDto;
 import study.datajpa.domain.member.entity.Member;
 import study.datajpa.domain.member.repository.MemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -27,8 +32,17 @@ public class MemberController {
         return member.getUserName();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 3) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("hoon", 29));
+
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
--- src/main/java/study/datajpa/domain/member/dto/MemberDto.java
+++ src/main/java/study/datajpa/domain/member/dto/MemberDto.java
@@ -2,6 +2,7 @@ package study.datajpa.domain.member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import study.datajpa.domain.member.entity.Member;
 
 @Data
 @AllArgsConstructor
@@ -9,5 +10,11 @@ public class MemberDto {
 
     private Long id;
     private String userName;
+
     private String teamName;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.userName = member.getUserName();
+    }
 }
