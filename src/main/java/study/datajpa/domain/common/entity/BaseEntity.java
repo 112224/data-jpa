8eb744630f95b07a7e70389d831234b1278cceaa
112224 <didgns25@naver.com>
2023-03-07 04:28:27 +0900

    Fix : 공통 엔티티 코딩 오류 수정
    BaseTimeEntity 에 시간과 관련된 값이 모이도록 수정

--- src/main/java/study/datajpa/domain/common/entity/BaseEntity.java
+++ src/main/java/study/datajpa/domain/common/entity/BaseEntity.java
@@ -3,20 +3,18 @@ package study.datajpa.domain.common.entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
 public class BaseEntity extends BaseTimeEntity{
 
-
+    @CreatedBy
+    private String createdBy;
     @LastModifiedBy
     private String lastModifiedBy;
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
+
 }
--- src/main/java/study/datajpa/domain/common/entity/BaseTimeEntity.java
+++ src/main/java/study/datajpa/domain/common/entity/BaseTimeEntity.java
@@ -1,11 +1,12 @@
 package study.datajpa.domain.common.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -15,10 +16,11 @@ import java.time.LocalDateTime;
 @Getter
 public class BaseTimeEntity {
 
-    @CreatedBy
-    @Column(updatable = false)
-    private String createdBy;
-    @CreatedDate
+    @JsonIgnore @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdDate;
+
+    @JsonIgnore @LastModifiedDate
+    @Column(updatable = false)
+    private LocalDateTime lastModifiedDate;
 }
